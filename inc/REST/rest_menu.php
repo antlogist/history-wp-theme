1efe885914a039fe6a3f8fddb10004e283dc8645
antlogist <antlogist@gmail.com>
2021-10-29 14:37:52 +0300

    Rest menu created

---
+++ inc/REST/rest_menu.php
@@ -0,0 +1,26 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined ('ABSPATH') ) {
+    exit;
+}
+
+function chip_menu() {
+  register_rest_route( 'menus/v1', 'menu', array(
+    'methods' => WP_REST_SERVER:: READABLE,
+    'callback' => 'get_menu',
+  ) );
+
+  register_rest_route( 'menus/v1', 'footer-menu', array(
+    'methods' => WP_REST_SERVER:: READABLE,
+    'callback' => 'get_footer_menu',
+  ) );
+}
+
+function get_menu() {
+  return wp_get_nav_menu_items('main');
+}
+
+function get_footer_menu() {
+  return wp_get_nav_menu_items('footer');
+}
