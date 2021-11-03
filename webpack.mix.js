e960920753a6c7b30df51ec89778f0651137d0b8
antlogist <antlogist@gmail.com>
2021-11-03 18:18:11 +0300

    Pdf section fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,11 +3,11 @@ const mix = require("laravel-mix");
 //mix.copy('images/', 'dist/images/', false);
 
 mix.sass("resources/assets/sass/app.scss", "dist/css/all.css");
-mix.sass("resources/assets/sass/libs.scss", "dist/css/libs.css");
+// mix.sass("resources/assets/sass/libs.scss", "dist/css/libs.css");
 
-mix.js([
-  "resources/assets/js/customization/theme-customize.js"
-], "dist/js/theme-customize.js");
+// mix.js([
+//   "resources/assets/js/customization/theme-customize.js"
+// ], "dist/js/theme-customize.js");
 
 mix.js([
   "resources/assets/js/baseObject.js",
@@ -16,11 +16,10 @@ mix.js([
   "resources/assets/js/init.js"
 ], "dist/js/all.js");
 
-//pdf
-mix.copy("resources/assets/libs/pdf/pdf.js", "dist/js/pdf/");
-mix.copy("resources/assets/libs/pdf/pdf.worker.js", "dist/js/pdf/");
-mix.copy("resources/assets/libs/pdf-legacy/pdf.js", "dist/js/pdf-legacy/");
-mix.copy("resources/assets/libs/pdf-legacy/pdf.worker.js", "dist/js/pdf-legacy/");
+// mix.copy("resources/assets/libs/pdf/pdf.js", "dist/js/pdf/");
+// mix.copy("resources/assets/libs/pdf/pdf.worker.js", "dist/js/pdf/");
+// mix.copy("resources/assets/libs/pdf-legacy/pdf.js", "dist/js/pdf-legacy/");
+// mix.copy("resources/assets/libs/pdf-legacy/pdf.worker.js", "dist/js/pdf-legacy/");
 
 mix.minify(['dist/css/all.css', 'dist/css/libs.css']);
 mix.minify(['dist/js/theme-customize.js', 'dist/js/all.js', 'dist/js/pdf/pdf.js', 'dist/js/pdf-legacy/pdf.js']);
