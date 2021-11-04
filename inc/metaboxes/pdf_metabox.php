697ceb83e24c20b83dd8f0f85cce360ec1187a29
antlogist <antlogist@gmail.com>
2021-11-04 08:42:09 +0300

    Cryptographic nonce added to the meta box saving process

--- inc/metaboxes/pdf_metabox.php
+++ inc/metaboxes/pdf_metabox.php
@@ -21,6 +21,7 @@ add_action( 'add_meta_boxes', 'add_pdf_meta_boxes' );
 
 
 function render_pdf_custom_box( $post ) {
+  wp_nonce_field(basename(__FILE__), 'custom_pdf_nonce' );
   $pdf = get_post_meta($post->ID, 'custom_pdf', true);
   ?>
   <div style="margin: 1rem 0;">
@@ -32,12 +33,21 @@ function render_pdf_custom_box( $post ) {
 }
 
 function save_pdf_postdata($post_id) {
-    if (array_key_exists('custom_pdf', $_POST)) {
-        update_post_meta(
-            $post_id,
-            'custom_pdf',
-            esc_url_raw($_POST['custom_pdf'])
-        );
-    }
+
+  $is_autosave = wp_is_post_autosave($post_id);
+  $is_revision = wp_is_post_revision($post_id);
+  $is_valid_nonce = (isset($_POST['custom_pdf_nonce']) && wp_verify_nonce($_POST['custom_pdf_nonce'], basename(__FILE__))) ? true : false;
+
+  if($is_autosave || $is_revision || !$is_valid_nonce) {
+    return;
+  }
+
+  if (array_key_exists('custom_pdf', $_POST)) {
+    update_post_meta(
+        $post_id,
+        'custom_pdf',
+        esc_url_raw($_POST['custom_pdf'])
+    );
+  }
 }
 add_action('save_post', 'save_pdf_postdata');
