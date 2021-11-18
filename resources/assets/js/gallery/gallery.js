02a5ef94f07c45e7abec279602389d1df716c3e8
antlogist <antlogist@gmail.com>
2021-11-18 10:23:59 +0300

    Gallery buttons and key listener added

--- resources/assets/js/gallery/gallery.js
+++ resources/assets/js/gallery/gallery.js
@@ -6,9 +6,23 @@
     const modal = document.querySelector(".archive-gallery-modal");
     const wrapper = document.querySelector(".gallery-outer-wrapper");
     const imgContainer = document.querySelector(".archive-gallery-img-container");
+    const imgAll = wrapper.querySelectorAll("img");
+
+    const imgArr = [];
+
+    [...imgAll].map((item) => {
+      const url = item.dataset.url;
+      imgArr.push(url);
+    });
+
+    const imgArrLength = imgArr.length;
+
+    let currImgIndex;
+
     wrapper.addEventListener("click", function(e) {
       e.preventDefault();
       const target = e.target;
+      const currentTarget = e.currentTarget;
       if(target.classList.contains("archive-gallery-img")) {
         if(modal.classList.contains("active")) {
           return;
@@ -16,7 +30,11 @@
 
         imgContainer.innerHTML = "";
         const imgUrl = target.dataset.url;
-        const htmlEl = `<div class="modal-img-outer-wrapper"><img class="w-100" src="${imgUrl}"></div>`;
+        const imgIndex = target.dataset.index;
+
+        currImgIndex = imgIndex;
+
+        const htmlEl = `<div class="modal-img-outer-wrapper"><img class="w-100 modal-img" src="${imgUrl}"></div>`;
         imgContainer.insertAdjacentHTML("afterbegin", htmlEl);
 
         body.classList.add("active");
@@ -27,7 +45,60 @@
         body.classList.remove("active");
         modal.classList.remove("active");
       }
-    })
+
+      const modalImgEl = document.querySelector(".modal-img");
+
+      //Left
+      if(target.classList.contains("left-button") || target.parentElement.classList.contains("left-button")) {
+
+        if (currImgIndex == 0 ) {
+          currImgIndex = imgArrLength;
+        }
+
+        modalImgEl.src = imgArr[--currImgIndex];
+      }
+
+      //Right
+      if(target.classList.contains("right-button") || target.parentElement.classList.contains("right-button")) {
+
+        if (currImgIndex == imgArrLength - 1 ) {
+          currImgIndex = -1;
+        }
+
+        modalImgEl.src = imgArr[++currImgIndex];
+      }
+
+    });
+
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+
+        e = e || window.event;
+
+        const modalImgEl = document.querySelector(".modal-img");
+
+        if(imgArrLength < 2) {
+          return;
+        }
+
+        //left arrow
+        if (e.keyCode == "37") {
+          if (currImgIndex == 0 ) {
+            currImgIndex = imgArrLength;
+          }
+          modalImgEl.src = imgArr[--currImgIndex];
+        }
+        //right arrow
+        else if (e.keyCode == "39") {
+          if (currImgIndex == imgArrLength - 1 ) {
+            currImgIndex = -1;
+          }
+
+          modalImgEl.src = imgArr[++currImgIndex];
+        }
+    }
   }
 
 })();
