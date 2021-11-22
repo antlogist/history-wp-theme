dc80fc319958acbefa81dd07851b32855c86db05
antlogist <antlogist@gmail.com>
2021-11-22 14:46:34 +0300

    Shipping types remdering function added

--- resources/assets/js/checkout/checkout.js
+++ resources/assets/js/checkout/checkout.js
@@ -15,31 +15,102 @@ BASEOBJECT.checkout.init = function () {
       fail: false,
       authenticated: false,
       isFirstLoading: true,
-      isLoading: false
+      isProductsLoading: true,
+      isShippingLoading: true,
+      isLoading: true,
+      shippingTypes: []
     },
     methods: {
       displayItems() {
         this.isLoading = true;
-        axios.get(`${themeUrl}/inc/app/Routes/GetCartItems.php`).then (function(response){
-          console.log(response);
-          if(response.data.fail) {
-            app.fail = true;
-            app.message = response.data.fail;
-          } else {
-            app.items = response.data.items;
-            app.cartTotal = response.data.cartTotal;
-            app.cartTotalVat = response.data.cartTotalVat;
-            app.vat = response.data.vat;
-            app.authenticated = response.data.authenticated;
-            app.currency = response.data.currency;
-          }
+        this.isProductsLoading = true;
+        this.isShippingLoading = true;
+
+        const shippingCountry = document.querySelector("#shippingCountry").value;
+        const shippingZipcode = document.querySelector("#shippingZipcode").value;
+
+
+        function getCartItems() {
+          return axios.get(`${themeUrl}/inc/app/Routes/GetCartItems.php`).then (function(response){
+            if(response.data.fail) {
+              app.fail = true;
+              app.message = response.data.fail;
+            } else {
+              app.items = response.data.items;
+              app.cartTotal = response.data.cartTotal;
+              app.cartTotalVat = response.data.cartTotalVat;
+              app.vat = response.data.vat;
+              app.authenticated = response.data.authenticated;
+              app.currency = response.data.currency;
+            }
+            app.isProductsLoading = false;
+          });
+        }
+
+        function getShippingTypes() {
+          return axios.post(`${themeUrl}/inc/app/Routes/GetShippingType.php`, JSON.stringify(
+            {
+              "country": shippingCountry,
+              "post_code": shippingZipcode,
+              "items": app.items
+            }
+          )).then(function(resp) {
+            app.shippingTypes = resp.data;
+            app.isShippingLoading = false;
+          });
+        }
+
+        const display = async() => {
+          await getCartItems();
+          await getShippingTypes();
           app.isFirstLoading = false;
           app.isLoading = false;
-        })
+          console.log(app.shippingTypes);
+        }
+
+        display();
+
       },
+      placeOrder() {
+
+        const data = {};
+
+        const profileForm = document.querySelector("#profileInfo").elements;
+        [...profileForm].map((item) => {
+          data[item.name] = item.value;
+        });
+
+        data.checkboxes = [];
+        const checkboxItems = document.querySelector("#checkboxesForm").elements;
+        [...checkboxItems].map(({name, value}) => {
+          data.checkboxes.push({
+            [name]: value
+          })
+        });
+
+        axios.post(`${themeUrl}/inc/app/Routes/Checkout.php`, JSON.stringify(
+          data
+        )).then(function(resp) {
+          console.log(resp);
+        });
+
+      }
     },
     created: function(){
       this.displayItems();
+    },
+    mounted() {
+      const shippingCountry = document.querySelector("#shippingCountry");
+      const shippingZipcode = document.querySelector("#shippingZipcode");
+
+      shippingCountry.addEventListener("change", function(e) {
+        app.displayItems();
+      });
+
+      shippingZipcode.addEventListener("change", function(e) {
+        app.displayItems();
+      });
+
     }
   });
 }
