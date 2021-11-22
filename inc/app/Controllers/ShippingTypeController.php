a0c4d98258894c94ec724b01547a920b8dde9433
antlogist <antlogist@gmail.com>
2021-11-22 13:33:52 +0300

    Shipping type controller created

---
+++ inc/app/Controllers/ShippingTypeController.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once("../../../../../../wp-config.php");
+include_once('../Classes/Request.php');
+include_once('../Classes/CSRFToken.php');
+include_once('../Classes/Redirect.php');
+include_once('../Classes/Session.php');
+
+class ShippingTypeController {
+  public $types = [];
+  private $url = api_url . '/api/v1/get-shipping-types?token=' . api_token;
+
+  private function typesRequest($country, $weight, $postCode, $productIds) {
+    $data = [
+      "weight" => $weight,
+      "country" => $country,
+      "product_ids" => $productIds,
+      "post_code" => $postCode
+    ];
+
+    $data_string = json_encode ($data);
+
+    $ch = curl_init ($this->url);
+    curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt ($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen ($data_string) ]
+    );
+    $output = curl_exec ($ch);
+
+    $this->types = json_decode ($output)->data;
+    curl_close($ch);
+
+  }
+
+  public function getTypes() {
+    $request = json_decode(file_get_contents('php://input'), true);
+    $request = (object) $request;
+
+    $products = $request->items;
+
+    $weight = 0;
+    $productIds = [];
+
+    foreach($products as $product) {
+      if ($product['weight']) {
+        $weight = $weight + $product['weight'];
+      }
+      array_push($productIds, $product['id']);
+    }
+
+    $this->typesRequest($request->country, $weight, $request->post_code, $productIds);
+
+    echo json_encode($this->types);
+  }
+}
