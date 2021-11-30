459884ec387f1bb1a2f7e7989a2261d04b888096
antlogist <antlogist@gmail.com>
2021-11-30 09:40:30 +0300

    Scripts and styles included

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,37 +11,37 @@ function history_styles_and_scripts() {
   wp_enqueue_style('dashicons');
 
   //libs CSS
-  wp_enqueue_style('history-lib-css', get_theme_file_uri('dist/css/libs.min.css'));
+  wp_enqueue_style('history-lib-css', get_theme_file_uri('dist/css/libs.min.css'), null, null);
 
   //custom CSS
-  wp_enqueue_style('history-app-css', get_theme_file_uri('dist/css/all.css'), null, microtime());
+  wp_enqueue_style('history-app-css', get_theme_file_uri('dist/css/all.min.css'), null, null);
 
-  //pdf js
-  if(is_front_page() || 'newsletter' === get_post_type()) {
-    // wp_enqueue_script('pdf-app-js', get_theme_file_uri('dist/js/pdf/pdf.min.js'), null, microtime(), false);
-    wp_enqueue_script('pdf-app-js', get_theme_file_uri('dist/js/pdf-legacy/pdf.min.js'), null, microtime(), false);
+  if(is_front_page()) {
+    //font
+    wp_enqueue_style('abril-font', 'https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap', null, null);
+  }
+
+  if('newsletter' === get_post_type() || 'will' === get_post_type()) {
+    //wp_enqueue_script('pdf-app-js', get_theme_file_uri('dist/js/pdf/pdf.min.js'), null, null, false);
+    wp_enqueue_script('pdf-app-js', get_theme_file_uri('dist/js/pdf-legacy/pdf.min.js'), null, null, false);
   }
 
   //custom js
-  wp_enqueue_script('history-app-js', get_theme_file_uri('dist/js/all.js'), array( 'jquery' ), microtime(), true);
+  wp_enqueue_script('history-app-js', get_theme_file_uri('dist/js/all.min.js'), array( 'jquery' ), null, true);
 
   //masonry js
   wp_enqueue_script('masonry-js', get_theme_file_uri('dist/js/masonry.pkgd.min.js'), null , '' , true);
 
-  //VUE for CPT
-  // if ('newsletter' === get_post_type()) {
-  //   if (wp_script_is(get_theme_file_uri('dist/js/vue/vue.js'), 'enqueued')) {
-  //     return;
-  //   } else {
-  //     wp_enqueue_script('vue-js', get_theme_file_uri('dist/js/vue/vue.js'), null , '' , false);
-  //   }
-  // }
+  //vue
+  if (is_page('shop') || is_page('cart') || is_page('product') || is_page('checkout') || is_page('view-order')) {
+    wp_enqueue_script('vue-js', get_theme_file_uri('dist/js/vue/vue.js'), null , '' , false);
+  }
 
 }
 
 function history_customizer_script() {
   //custom js
-  wp_enqueue_script('history-customizer-js', get_theme_file_uri('dist/js/theme-customize.min.js'), array( 'jquery','customize-preview' ), microtime(), true);
+  wp_enqueue_script('history-customizer-js', get_theme_file_uri('dist/js/theme-customize.min.js'), array( 'jquery','customize-preview' ), null, true);
 }
 
 function history_admin_scripts() {
@@ -50,10 +50,10 @@ function history_admin_scripts() {
   }
 
   //metaboxes JS
-  wp_enqueue_script('history-pdf-metabox', get_theme_file_uri('dist/admin/js/pdf/pdfMetabox.js'), array('jquery'), microtime(), false);
-  wp_enqueue_script('history-gallery-metabox', get_theme_file_uri('dist/admin/js/gallery/galleryMetabox.min.js'), array('jquery'), microtime(), false);
+  wp_enqueue_script('history-pdf-metabox', get_theme_file_uri('dist/admin/js/pdf/pdfMetabox.js'), array('jquery'), null, false);
+  wp_enqueue_script('history-gallery-metabox', get_theme_file_uri('dist/admin/js/gallery/galleryMetabox.min.js'), array('jquery'), null, false);
 
   //admin styles
-  wp_enqueue_style('history-app-css', get_theme_file_uri('dist/admin/css/app.min.css'), null, microtime());
+  wp_enqueue_style('history-app-css', get_theme_file_uri('dist/admin/css/app.min.css'), null, null);
 
 }
