35629d894fb4c234f83e06c708408f895b125b64
antlogist <antlogist@gmail.com>
2022-03-03 14:51:18 +0300

    User class extended

--- inc/app-membership/Controllers/UserController.php
+++ inc/app-membership/Controllers/UserController.php
@@ -1,46 +1,8 @@
 <?php
 include_once('../Database/Database.php');
-// include_once('../../app/Classes/Request.php');
-// include_once('../../app/Classes/CSRFToken.php');
-// include_once('../../app/Classes/Redirect.php');
-// include_once('../../app/Classes/Session.php');
+include_once('User.php');
 
-class UserController {
-  private $conn;
-  private $table = 'users';
-  private $email = '';
-
-  public function __construct() {
-
-    if(!isAuthenticated()) {
-      Session::add('error', "Malicious Activity");
-      exit;
-    }
-
-    $uuid = Session::get('SESSION_USER_UUID');
-    $api_url = api_url . '/api/v1/get-profile?token=' . api_token;
-    $data = ["uuid" => $uuid];
-    $data_string = json_encode ($data);
-    $ch = curl_init ($api_url);
-    curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt ($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt ($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen ( $data_string ) ]
-    );
-    $output = curl_exec ($ch);
-    $this->email = json_decode ($output)->data->email;
-
-    if($this->email === ''){
-      Session::add('error', "Malicious Activity");
-      exit;
-    }
-
-    $database = Database::getInstance();
-    $this->conn = $database->conn;
-  }
-
+class UserController extends User {
 
   public function show() {
 
